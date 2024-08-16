3aed72d224986cb62b357270bf463eec006f7e55
Harikrishnanks312 <118618866+Harikrishnanks312@users.noreply.github.com>
2024-08-17 02:20:01 +0530

    xox update

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -5,7 +5,8 @@ import axios from 'axios';
 import useAuth from '../../../hooks/useAuth';
 
 export default function Home() {
-    const baseURL = "http://localhost:3001"
+    // const baseURL = "http://localhost:3001"
+    // const baseURL = "https://xox-backend.onrender.com"
 
     const [join, setJoin] = useState(false);
     const [boardId, setBoardId] = useState('')
